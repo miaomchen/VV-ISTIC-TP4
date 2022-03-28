fb27edf0ae72985d01675f3e4384a50fd240bb08
miaomchen <75794344+miaomchen@users.noreply.github.com>
2022-03-29 00:42:01 +0200

    Update RomanNumeralTest.java

--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -2,8 +2,28 @@ package fr.istic.vv;
 import net.jqwik.api.*;
 
 public class RomanNumeralTest {
+
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    boolean parseToRomanNumeral(@ForAll("symbols") String aString) {
+        int parsed = RomanNumeraUtils.parseRomanNumeral(aString);
+        return RomanNumeraUtils.toRomanNumeral(parsed).equals(aString);
     }
+
+    @Property
+    boolean toParseRomanNumeral(@ForAll("numbers") int anInteger) {
+        String toNumeral = RomanNumeraUtils.toRomanNumeral(anInteger);
+        return RomanNumeraUtils.parseRomanNumeral(toNumeral) == anInteger;
+    }
+
+    @Provide
+    Arbitrary<String> symbols() {
+       return Arbitraries.strings()
+                .withChars('M', 'D', 'C', 'X', 'V', 'I').ofMinLength(1);
+    }
+
+    @Provide
+    Arbitrary<Integer> numbers() {
+        return Arbitraries.integers().between(1, 3999);
+    }
+
 }
